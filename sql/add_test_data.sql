ed18a1cd7f5559077f2a978bcd6e8004632b17d9
sandral <anni.luhtaniemi@helsinki.fi>
2014-01-26 15:51:51 +0200

    muoks

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,10 +1,8 @@
-INSERT INTO users (username, password, isadmin) values ("Pentti", "merinovillahousut", 1)
+INSERT INTO users (username, password, isadmin) values ('Pentti', 'merinovillahousut', 1);
 
-INSERT INTO yarn (yarnname, nsrmin, nsrmax, lpg) values ("Alpaca", 30, 35, 420)
-("Alpakka", 50, 60, 240)
-("Cora", 40, 50, 170)
+INSERT INTO yarn (yarnname, nsrmin, nsrmax, lpg) values ('Alpaca', 30, 35, 420),('Alpakka', 50, 60, 240),('Cora', 40, 50, 170);
 
-INSERT INTO manu (manuname) values ("Drops")("TeeTee")("Linie")
+INSERT INTO manu (manuname) values ('Drops'),('TeeTee'),('Linie');
 
-INSERT INTO attr (attrname) values ("merinovilla") ("akryyli") ("alpakka") ("valkoinen") ("punainen") ("lila")
+INSERT INTO attr (attrname) values ('merinovilla'),('akryyli'),('alpakka'),('valkoinen'),('punainen'),('lila');
 
