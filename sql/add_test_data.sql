c82bad54ad395de7e8f78b2efc60c523647c1eb2
sandral <anni.luhtaniemi@helsinki.fi>
2014-01-26 16:47:44 +0200

    asd

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,22 +1,16 @@
-INSERT INTO users (username, password, isadmin) values ('Pentti', 'merinovillahousut', 1);
+INSERT INTO users (username, password, isadmin) values ('Pentti', 'merinovillahousut', 1), ('Anneli', 'asdf', 0);
 
 INSERT INTO manu (manuname) values ('Drops'),('TeeTee'),('Linie');
-
-/*INSERT INTO yarn (yarnname, yarnmanu, nsrmin, nsrmax, lpg)
-values ('Alpaca', 30, 35, 420),('Alpakka', 50, 60, 240),('Cora', 40, 50, 170);
-*/
 
 INSERT INTO yarn (yarnname, yarnmanu, nsrmin, nsrmax, lpg)
 (select 'Alpaca', manu_id, 30, 35, 420 from manu where manuname='Drops')
 union (select 'Alpakka', manu_id, 50, 60, 240 from manu where manuname='TeeTee')
 union (select 'Cora', manu_id, 40, 50, 170 from manu where manuname='Linie');
 
-/*
-INSERT INTO yarn (yarnname, yarnmanu, nsrmin, nsrmax, lpg)
-select 'Alpaca
-*/
-
 INSERT INTO attr (attrname) values ('merinovilla'),('akryyli'),('alpakka'),('valkoinen'),('punainen'),('lila');
- 
+
 INSERT INTO yarnattr (yarn, attr)
-select yarn_id, attr_id from yarn, attr;
+select yarn_id, attr_id from yarn, attr where
+(yarnname='Alpaca' and attrname in ('alpakka', 'valkoinen')) or
+(yarnname='Alpakka' and attrname in ('alpakka', 'punainen')) or
+(yarnname='Cora' and attrname in ('merinovilla', 'akryyli', 'lila'));
