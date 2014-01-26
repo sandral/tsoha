603ea25f1a2dde02a4f2c12b948e75719f60175f
sandral <anni.luhtaniemi@helsinki.fi>
2014-01-26 16:29:24 +0200

    asd

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,8 +1,15 @@
 INSERT INTO users (username, password, isadmin) values ('Pentti', 'merinovillahousut', 1);
 
-INSERT INTO yarn (yarnname, nsrmin, nsrmax, lpg) values ('Alpaca', 30, 35, 420),('Alpakka', 50, 60, 240),('Cora', 40, 50, 170);
-
 INSERT INTO manu (manuname) values ('Drops'),('TeeTee'),('Linie');
+
+/*INSERT INTO yarn (yarnname, yarnmanu, nsrmin, nsrmax, lpg)
+values ('Alpaca', 30, 35, 420),('Alpakka', 50, 60, 240),('Cora', 40, 50, 170);
+*/
+
+INSERT INTO yarn (yarnname, yarnmanu, nsrmin, nsrmax, lpg)
+select 'Alpaca', manu_id, 30, 35, 420 from manu where manuname='Drops';
+
+
 
 INSERT INTO attr (attrname) values ('merinovilla'),('akryyli'),('alpakka'),('valkoinen'),('punainen'),('lila');
  
