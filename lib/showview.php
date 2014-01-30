5b99f680d6a11a6480e8dd13d0250f638df1792a
sandral <anni.luhtaniemi@helsinki.fi>
2014-01-30 21:10:34 +0200

    asdf

--- lib/showview.php
+++ lib/showview.php
@@ -1,9 +1,6 @@
 <?php
 function showView($sivu, $data = array()) {
-	 echo 789;
     $data = (object)$data;
-echo 333;
-    include '../views/template.php';
-    echo 111;
+    require 'views/template.php';
     die();
 }
