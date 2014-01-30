f81e162881e13414e46fb2790c4837d688a8573b
sandral <anni.luhtaniemi@helsinki.fi>
2014-01-30 22:05:48 +0200

    asdf

--- views/template.php
+++ views/template.php
@@ -1,8 +1,16 @@
 <!DOCTYPE HTML>
 <html>
 <head><title>Otsikko</title>
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap-theme.css" rel="stylesheet">
+<link href="css/main.css" rel="stylesheet">
 </head>
 <body>
+<?php
+if (isset($data->error)){
+echo '<div class="error">'.($data->error).'</div>';
+}
+?>
 <div id="content">
 <?php require $sivu; ?>
 </div>
