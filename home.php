b1d9a0ccc0394e9b222133653848ca99bd2e6856
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-01 21:47:15 +0200

    asd

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ require_once 'lib/class_user.php';
 
 if (logged()){
 echo "Kirjautunut:";
-echo var_export(loggedUser());
+echo var_export(loggedUser()->getUsername());
 } else {
 echo "Not logged!";
 }
