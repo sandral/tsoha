f0c47bd399ccba79b49cc7eb43e9405578715955
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-04 22:26:03 +0200

    asd

--- home.php
+++ home.php
@@ -4,8 +4,15 @@ require_once 'lib/class_user.php';
 require_once 'lib/lib.php';
 
 if (logged()){
-  $loggedUser = loggedUser();
-  echo $loggedUser;
+  $loggedUser = 45;
+
+showView('views/home.php', array
+(
+'user' => $loggedUser
+)
+);
+
+
 } else {
   header('Location: login.php');
   exit();
