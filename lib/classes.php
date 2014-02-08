dd79feee4634d035d136733dad006851b922811d
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 19:05:54 +0200

    asd

--- lib/classes.php
+++ lib/classes.php
@@ -3,3 +3,4 @@
 require_once 'class_user.php';
 require_once 'class_yarn.php';
 require_once 'class_owns.php';
+require_once 'class_manu.php';
--- yarn.php
+++ yarn.php
@@ -13,14 +13,14 @@ if (!isset($_GET['yarn_id'])){
 
 $yarn_id = (int)$_GET['yarn_id'];
 $yarn = Yarn::getYarnById($yarn_id);
-echo 1;
+
 if (is_null($yarn)) {
    header('Location: home.php');
    exit();
 }
-echo 2;
+
 $manu = Manu::getManuById($yarn->getYarnmanu());
-echo 3;
+
 showView('views/yarn.php', array
 (
 'user' => $loggedUser->getUsername(),
