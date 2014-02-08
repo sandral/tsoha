1277888220ec8382bfe80ac92837a8eb800506c0
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 23:21:43 +0200

    asd

--- views/yarn.php
+++ views/yarn.php
@@ -16,6 +16,7 @@
 <tr><td>Pituus (100g):</td><td> <input type="text" name="lpg" value="<?php echo $data->yarn->getLpg();?>"></td></tr>
 <tr><td>Kuvaus:</td><td>        <input type="text" name="description" value="<?php echo $data->yarn->getDescription();?>"></td></tr>
 </table>
+<input type="submit" value="Muokkaa">
 </form>
 
 <br>
