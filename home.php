37f798c84f3adb544075845d45e6fcb4c7c8376c
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 18:17:00 +0200

    asd

--- home.php
+++ home.php
@@ -3,11 +3,11 @@
 require_once 'lib/lib.php';
 require_once 'lib/classes.php';
 
+echo 1;
 if (logged()){
   $loggedUser = loggedUser();
 
   $owns = $loggedUser->getOwned();
-  echo 1;
 
   showView('views/home.php', array('user' => $loggedUser->getUsername(), 'owns' => $owns));
 
