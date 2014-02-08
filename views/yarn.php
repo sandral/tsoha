9db22f3e8fd4c0d9ecf7829b4a3ffbab7f0ac2a0
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 20:20:40 +0200

    asd

--- views/yarn.php
+++ views/yarn.php
@@ -1,6 +1,8 @@
-<h1>Langan tiedot</h1>
+<h1>Muokkaa langan tietoja</h1>
 
-<form action="yarn.php">
+<form action="yarn.php" method="POST">
+<input type="hidden" name="action" value="edit">
+<input type="hidden" name="yarn_id" value="<?php echo $data->yarn->getId(); ?>">
 <table>
 <tr><td>Nimi:</td><td>          <input type="text" name="yarnname" value="<?php echo htmlspecialchars($data->yarn->getYarnname());?>"></td></tr>
 <tr><td>Valmistaja:</td><td>    <select name="yarnmanu">
@@ -10,7 +12,7 @@
 </option>
 
 </select></td></tr>
-<tr><td>Puikkosuositus:</td><td><?php showNsrfield('nsrmin',$data->yarn->getNsrmin());?> - <?php showNsrfield('nsrmax', $data->yarn->getNsrmax());?>"></td></tr>
+<tr><td>Puikkosuositus:</td><td><?php showNsrfield('nsrmin',$data->yarn->getNsrmin());?> - <?php showNsrfield('nsrmax', $data->yarn->getNsrmax());?></td></tr>
 <tr><td>Pituus (100g):</td><td> <input type="text" name="lpg" value="<?php echo $data->yarn->getLpg();?>"></td></tr>
 <tr><td>Kuvaus:</td><td>        <input type="text" name="description" value="<?php echo $data->yarn->getDescription();?>"></td></tr>
 </table>
