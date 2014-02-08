b1688c624a2b31841af5e2c304a0c34f10318f71
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 19:07:24 +0200

    asd

--- views/yarn.php
+++ views/yarn.php
@@ -3,7 +3,7 @@
 <form action="yarn.php">
 <table>
 <tr><td>Nimi:</td><td><?php echo $data->yarn->getYarnname();?></td></tr>
-<tr><td>Valmistaja:</td><td><?php echo $data->manu;?></td></tr>
+<tr><td>Valmistaja:</td><td><?php echo $data->manu->getManuname(); ?></td></tr>
 <tr><td>Puikkosuositus:</td><td><?php echo $data->yarn->getNsrmin();?> - <?php echo $data->yarn->getNsrmax();?></td></tr>
 <tr><td>Pituus (100g):</td><td><?php echo $data->yarn->getLpg();?></td></tr>
 <tr><td>Kuvaus:</td><td><?php echo $data->yarn->getDescription();?></td></tr>
