e5e8520fd54e3715bf497b2d3b3b6619570352e1
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 20:23:37 +0200

    asd

--- views/home.php
+++ views/home.php
@@ -2,16 +2,15 @@
 
 Omat lankasi:<br>
 <table>
-<tr><th>Nimi</th><th>Määrä</th></tr>
+<tr><th>Nimi</th><th>Määrä</th></tr><td></td>
 <?php
 foreach ($data->owns as $own) {
 echo '<tr>';
 echo '<td>';
-echo '<a href="yarn.php?yarn_id='.$own['yarn']->getId().'">';
 echo $own['yarn']->getYarnname().'</td><td>'.$own['amount'].'g';
-echo '</a>';
-echo '</td>';
-echo '</tr>';
+echo '</td><td>';
+echo '<a href="yarn.php?yarn_id='.$own['yarn']->getId().'">Muokkaa</a>';
+echo '</td></tr>';
  }
 ?>
 </table>
