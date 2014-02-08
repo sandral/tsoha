aa553f85f3983e549a717539c12469b100d9a847
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-08 19:28:17 +0200

    asd

--- views/yarn.php
+++ views/yarn.php
@@ -2,11 +2,11 @@
 
 <form action="yarn.php">
 <table>
-<tr><td>Nimi:</td><td><?php echo $data->yarn->getYarnname();?></td></tr>
-<tr><td>Valmistaja:</td><td><?php echo $data->manu->getManuname(); ?></td></tr>
-<tr><td>Puikkosuositus:</td><td><?php echo $data->yarn->getNsrmin();?> - <?php echo $data->yarn->getNsrmax();?></td></tr>
-<tr><td>Pituus (100g):</td><td><?php echo $data->yarn->getLpg();?></td></tr>
-<tr><td>Kuvaus:</td><td><?php echo $data->yarn->getDescription();?></td></tr>
+<tr><td>Nimi:</td><td><input type="text" value="<?php echo htmlspecialchars($data->yarn->getYarnname());?>"></td></tr>
+<tr><td>Valmistaja:</td><td><input type="text" value="<?php echo htmlspecialchars($data->manu->getManuname());?>"></td></tr>
+<tr><td>Puikkosuositus:</td><td><input type="text" value="<?php echo $data->yarn->getNsrmin();?>"> - <input type="text" value="<?php echo $data->yarn->getNsrmax();?>"></td></tr>
+<tr><td>Pituus (100g):</td><td><input type="text" value="<?php echo $data->yarn->getLpg();?>"></td></tr>
+<tr><td>Kuvaus:</td><td><input type="text" value="<?php echo $data->yarn->getDescription();?>"></td></tr>
 </table>
 </form>
 
