b45c28170fda6c25a331c2a2f3b83326e198a69d
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-09 12:37:42 +0200

    asd

--- addyarn.php
+++ addyarn.php
@@ -9,10 +9,12 @@ $loggedUser = loggedUser();
 if (isset($_POST['action']) && $_POST['action']=='insert'){
    $yarnname = $_POST['yarnname'];
    $yarnmanu = (int) $_POST['yarnmanu'];
-   $nsrmin = (int) $_POST['nsrmin'];
-   $nsrmax = (int) $_POST['nsrmax'];
+   $nsrmin = $_POST['nsrmin'];
+   $nsrmax = $_POST['nsrmax'];
    $lpg = (int) $_POST['lpg'];
    $description = $_POST['description'];
+
+   echo $yarnname.$yarnmanu.$nsrmin.$nsrmax.$lpg.$description;
 
    Yarn::addYarn($yarnname, $yarnmanu, $nsrmin, $nsrmax, $lpg, $description);
    redirect('home.php');
