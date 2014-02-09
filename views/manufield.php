9f4da2c13bfd10877475d3261d5a2d85940d2a4e
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-09 12:24:48 +0200

    asd

--- views/manufield.php
+++ views/manufield.php
@@ -3,12 +3,11 @@
 <?php
 $manus = Manu::listManus();
 foreach ($manus as $manu) {
-  echo '<option value="'.$manu->getId().
-       '"';
+  echo '<option value="'.$manu->getId().'"';
   if ($manufieldselected == $manu->getId()){
     echo ' selected';
   }
-  echo '>'.$manu->getName().'</option>';
+  echo '>'.$manu->getManuname().'</option>';
 }
 ?>
 
