36f6848f0091001e29f581ae3d508ced78abb9ad
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-09 12:22:01 +0200

    asd

--- lib/class_manu.php
+++ lib/class_manu.php
@@ -13,6 +13,21 @@ class Manu {
   public function getId() { return $this->manu_id; }
   public function getManuname() { return $this->manuname; }
 
+  public static function listManus() {
+    $sql = 'SELECT * FROM manu ORDER BY manuname ASC';
+    $query = getTietokantayhteys()->prepare($sql);
+    $query->execute(array());
+
+    $ret = array();
+    foreach($query->fetchAll(PDO::FETCH_OBJ) as $res) {
+      $manu = new Manu($res->manu_id, $res->manuname);
+      $ret[] = $manu;
+    }
+
+    return $ret;
+  }
+
+
   public static function getManuById($id) {
     $sql = "SELECT * FROM manu WHERE manu_id = ? LIMIT 1";
     $query = getTietokantayhteys()->prepare($sql);
--- views/manufield.php
+++ views/manufield.php
@@ -1,6 +1,15 @@
-<select name="yarnmanu">
+<select name="<?php echo $manufieldname;?>">
 
-<option value="">
-</option>
+<?php
+$manus = Manu::listManus();
+foreach ($manus as $manu) {
+  echo '<option value="'.$manu->getId().
+       '"';
+  if ($manufieldselected == $manu->getId()){
+    echo ' selected';
+  }
+  echo '>'.$manu->getName().'</option>';
+}
+?>
 
 </select>
