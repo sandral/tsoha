00f5d369d5c12249354ae133c932fa6766b05e50
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-10 19:13:24 +0200

    asd

--- views/login.php
+++ views/login.php
@@ -4,11 +4,11 @@
   <span class="input-group-addon">Käyttäjänimi</span>
   <input type="text" class="form-control" name="user" value="<?php echo $data->user; ?>">
 </div>
-
+<br>
 <div class="input-group">
   <span class="input-group-addon">Salasana</span>
   <input type="password" class="form-control" name="pwd">
 </div>
-
+<br>
 </form>
 <a href="#" onclick="document.getElementById('loginform').submit();" class="btn btn-default">Kirjaudu sisään</a>
