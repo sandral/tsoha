5acef6d261b519fc48286ff6ef56a8cf5d40bf96
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-10 22:26:14 +0200

    asd

--- views/home.php
+++ views/home.php
@@ -3,11 +3,11 @@
 <?php
 foreach ($data->yarnlist as $own) {
 echo '<tr>';
-echo '<td>'.($own['yarn']->getYarnname()).'</td>';
+echo '<td>'.htmlspecialchars($own['yarn']->getYarnname()).'</td>';
 if ($own['manu'] == NULL) {
   echo '<td> </td>';
 } else {
-  echo '<td>'.($own['manu']->getManuname()).'</td>';
+  echo '<td>'.htmlspecialchars($own['manu']->getManuname()).'</td>';
 }
 
 echo '<td>'.($own['yarn']->getNsrmin()).' - '.($own['yarn']->getNsrmax()).'</td>';
@@ -17,7 +17,7 @@ if ($own['yarn']->getLpg() == null) {
   echo '<td>'.($own['yarn']->getLpg()).' m</td>';
 }
 
-echo '<td>'.($own['yarn']->getDescription()).'</td>';
+echo '<td>'.htmlspecialchars($own['yarn']->getDescription()).'</td>';
 echo '<td><a href="yarn.php?action=modify&yarn_id='.$own['yarn']->getId().'">Muokkaa</a></td>';
 echo '<td><a href="yarn.php?action=delete&yarn_id='.$own['yarn']->getId().'">Poista</a></td>';
 echo '</tr>
