6ed2b5eba7672d9661226bddbea9c177bb9c4d42
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-10 19:22:09 +0200

    asd

--- views/navbar.php
+++ views/navbar.php
@@ -1,12 +1,6 @@
-<nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-inverse" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
 <?php
 if (logged()){
   echo '<a class="navbar-brand" href="home.php">Lankatietokanta</a>';
