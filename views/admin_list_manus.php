12a8ce491488325c3135cf02ec3c7c8be22b77ac
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-11 21:22:45 +0200

    admin-ominaisuuksia

--- views/admin_list_manus.php
+++ views/admin_list_manus.php
@@ -1,9 +1,10 @@
+<a href="admin_manu.php?action=insert" class="btn btn-default">Lisää valmistaja</a><br>
 <table class="table">
 <tr><th>Nimi</th></tr>
 <?php
 foreach ($data->list as $e) {
 echo '<tr>';
-echo '<td>'.htmlspecialchars($e->getManuname()).'</td>';
+echo '<td>'.htmlspecialchars($e->getManuname()).'</td><td></td><td></td>';
 
 echo '<td><a href="admin_manu.php?action=modify&manu_id='.$e->getId().'">Muokkaa</a></td>';
 echo '<td><a href="admin_manu.php?action=delete&manu_id='.$e->getId().'">Poista</a></td>';
