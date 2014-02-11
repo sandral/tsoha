1539e56b1ede7fccbe71b7c3d3f1ff6ec0ee7cc9
sandral <anni.luhtaniemi@helsinki.fi>
2014-02-11 21:02:45 +0200

    admin-ominaisuuksia

--- views/admin_list_manus.php
+++ views/admin_list_manus.php
@@ -1,25 +1,12 @@
 <table class="table">
-<tr><th>Nimi</th><th>Valmistaja</th><th>Puikkosuositus</th><th>Pituus/100g</th><th>Kuvaus</th><td></td><td></td></tr>
+<tr><th>Nimi</th></tr>
 <?php
-foreach ($data->yarnlist as $own) {
+foreach ($data->list as $e) {
 echo '<tr>';
-echo '<td>'.htmlspecialchars($own['yarn']->getYarnname()).'</td>';
-if ($own['manu'] == NULL) {
-  echo '<td> </td>';
-} else {
-  echo '<td>'.htmlspecialchars($own['manu']->getManuname()).'</td>';
-}
+echo '<td>'.htmlspecialchars($e->getManuname()).'</td>';
 
-echo '<td>'.($own['yarn']->getNsrmin()).' - '.($own['yarn']->getNsrmax()).'</td>';
-if ($own['yarn']->getLpg() == null) {
-  echo '<td> </td>';
-} else {
-  echo '<td>'.($own['yarn']->getLpg()).' m</td>';
-}
-
-echo '<td>'.htmlspecialchars($own['yarn']->getDescription()).'</td>';
-echo '<td><a href="admin_yarn.php?action=modify&yarn_id='.$own['yarn']->getId().'">Muokkaa</a></td>';
-echo '<td><a href="admin_yarn.php?action=delete&yarn_id='.$own['yarn']->getId().'">Poista</a></td>';
+echo '<td><a href="admin_manu.php?action=modify&manu_id='.$e['manu']->getId().'">Muokkaa</a></td>';
+echo '<td><a href="admin_manu.php?action=delete&manu_id='.$e['manu']->getId().'">Poista</a></td>';
 echo '</tr>
 ';
  }
